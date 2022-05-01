b0fcd405114752172de9066b6913fd536cc1e80a
TwoFingersGames <79571545+TwoFingersGames@users.noreply.github.com>
2022-05-02 00:59:39 +0700

    2d sprite package

--- Assets/Scripts/App/LevelsLogic.cs
+++
@@ -1,7 +0,0 @@
-﻿public class LevelsLogic : Game
-{
-    private void Awake()
-    {
-        app.levelsLogic = gameObject.GetComponent<LevelsLogic>();
-    }
-}
--- Assets/Scripts/Objects/UI/Headbar/Life.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.UI;
-
-public class Life : Game
-{
-    private void Start()
-    {
-        gameObject.GetComponent<Text>().text = "x "+app.saveSerial.lives + " ";
-    }
-}
